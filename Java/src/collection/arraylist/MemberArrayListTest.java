d9b11872d94134f77c95b39cebd5ea99dcc2fe15
SuminAnn <annsumin2066@naver.com>
2022-08-18 17:47:48 +0900

    JAVA스터디(Collection_ArrayList)

---
+++ Java/src/collection/arraylist/MemberArrayListTest.java
@@ -0,0 +1,24 @@
+package collection.arraylist;
+
+import collection.Member;
+
+public class MemberArrayListTest {
+
+	public static void main(String[] args) {
+
+		MemberArrayList memberArryList = new MemberArrayList();
+		
+		Member memberLee = new Member(101, "이순신");
+		Member memberKim = new Member(102, "김유신");
+		Member memberShin = new Member(103, "신사임당");
+		
+		memberArryList.addMember(memberLee);
+		memberArryList.addMember(memberKim);
+		memberArryList.addMember(memberShin);
+		
+		memberArryList.showAll();
+		
+		memberArryList.removeMember(memberKim.getMemberID());
+		memberArryList.showAll();
+	}
+}
